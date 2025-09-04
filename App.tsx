8faab7a9efe1fdb39661b422660aab47132be5fc
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-09-04 09:58:13 -0300

    Atualiza estilos

--- App.tsx
+++ App.tsx
@@ -1,16 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ backgroundColor: "lightgreen", padding: 40 }}>
+      <SafeAreaView style={estilos.container}>
         <StatusBar style="auto" />
-        <View>
+        <View style={estilos.viewLogo}>
           <Text>Da hora Filmes</Text>
+        </View>
+        <View style={estilos.viewBotoes}>
+          <Text>Botão 1</Text>
+          <Text>Botão 2</Text>
+        </View>
+        <View style={estilos.viewRodape}>
+          <Text>Botão 3</Text>
+          <Text>Botão 4</Text>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+/* Estilos */
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff9c4",
+    paddingHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+    /* alignItems: "center", */
+  },
+  viewLogo: {
+    backgroundColor: "lightgreen",
+    flex: 2,
+  },
+  viewBotoes: {
+    backgroundColor: "orange",
+    flex: 1,
+  },
+  viewRodape: {
+    backgroundColor: "pink",
+    flex: 0.5,
+  },
+});
