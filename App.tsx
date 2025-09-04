a83d624fb68676b40c3c2b8d7fc47d9fa8731db3
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-09-04 11:32:21 -0300

    Carregamento de fonte externa

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    Monoton: require("./assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  // Se a fonte ainda não foi carregada
+  if (fonteCarregada) {
+    return (
+      <Text style={{ color: "red", fontSize: 22 }}>Carregando Fonte...</Text>
+    );
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={estilos.container}>
         <StatusBar style="auto" />
         <View style={estilos.viewLogo}>
+          <Image source={require("./assets/dahora.png")} style={estilos.logo} />
           <Text>Da hora Filmes</Text>
         </View>
         <View style={estilos.viewBotoes}>
@@ -26,17 +39,19 @@ export default function App() {
 /* Estilos */
 const estilos = StyleSheet.create({
   container: {
-    backgroundColor: "#fff9c4",
+    backgroundColor: "#fff",
     paddingHorizontal: 20,
     flex: 1,
-
     justifyContent: "center",
   },
   viewLogo: {
-    backgroundColor: "lightgreen",
     flex: 3,
-    justifyContent: "flex-end",
+    justifyContent: "center",
     alignItems: "center",
+  },
+  logo: {
+    width: 128,
+    height: 128,
   },
   viewBotoes: {
     backgroundColor: "orange",
