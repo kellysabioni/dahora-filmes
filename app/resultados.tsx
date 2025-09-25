bc446a2137990e87f58801c36d2d7a0a5b28faf3
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-09-25 08:54:16 -0300

    importa useState

--- app/resultados.tsx
+++ app/resultados.tsx
@@ -3,11 +3,13 @@ import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { ParametrosBusca } from "@/src/types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "@/src/services/api";
 
 export default function Resultados() {
   const { filme } = useLocalSearchParams<ParametrosBusca>();
+
+  const [resultados, setResultados] = useState<ParametrosBusca>();
 
   useEffect(() => {
     //Se não houver filme definido
