b0a111412f351310a5f92b293b1eaf0b0b879d81
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-09-25 10:45:39 -0300

    Atualiza

--- app/resultados.tsx
+++ app/resultados.tsx
@@ -6,6 +6,7 @@ import { Filme, ParametrosBusca } from "@/src/types";
 import { useEffect, useState } from "react";
 import { api } from "@/src/services/api";
 import Loading from "@/src/components/Loading";
+import CardFilme from "@/src/components/CardFilme";
 
 export default function Resultados() {
   const { filme } = useLocalSearchParams<ParametrosBusca>();
@@ -57,7 +58,10 @@ export default function Resultados() {
           <FlatList
             data={resultados}
             keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => <Text>{item.title}</Text>}
+            renderItem={({ item }) => <CardFilme filme={item} />}
+            numColumns={2}
+            columnWrapperStyle={estilos.coluna}
+            showsVerticalScrollIndicator={false}
             ListEmptyComponent={
               <Text key="empty">Nenhum filme encontrado! </Text>
             }
