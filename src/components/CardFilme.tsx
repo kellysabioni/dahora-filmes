3de68d9188ca686c2b3200d8fd58336719c73e06
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-09-26 08:28:19 -0300

    Atualiza

--- src/components/CardFilme.tsx
+++ src/components/CardFilme.tsx
@@ -4,9 +4,19 @@ import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 // @ts-ignore
 import { Ionicons } from "@expo/vector-icons";
 import { CardFilmeProps } from "../types";
+import { useRouter } from "expo-router";
 
 export default function CardFilme({ filme }: CardFilmeProps) {
   const { id, title, poster_path } = filme;
+
+  // Importando useRouter para permitir a navegação através de programação
+  const router = useRouter();
+
+  const leiaMais = () => {
+    router.push({
+      pathname: "/detalhes/[id]",
+    });
+  };
 
   return (
     <View style={estilos.card}>
@@ -21,7 +31,7 @@ export default function CardFilme({ filme }: CardFilmeProps) {
       <View style={estilos.corpo}>
         <Text style={estilos.titulo}> {title} </Text>
         <View style={estilos.botoes}>
-          <Pressable style={estilos.botao}>
+          <Pressable style={estilos.botao} onPress={leiaMais}>
             <Text style={estilos.textoBotao}>
               <Ionicons name="book" size={12} /> Leia mais
             </Text>
